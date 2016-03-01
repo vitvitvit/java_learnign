01d399f1931df1b9df16f3b33678bbf442f6aeb1
vitvitvit <vitvitvit82@mail.ru>
2016-03-01 22:48:13 +0300

    добавлено несколько функций

--- sandbox/src/main/java/ru/stqa/ptf/sandbox/My1program.java
+++ sandbox/src/main/java/ru/stqa/ptf/sandbox/My1program.java
@@ -3,7 +3,29 @@ package ru.stqa.ptf.sandbox;
 public class My1program {
 
   public static void main(String[] args) {
-    System.out.println("Hello, world!");
+    hello("world");
+    hello("user");
+    hello("xzkto");
+
+    double len = 5;
+    System.out.println("Площадь квадрата со стороной " + len + " = " + area(len));
+
+    double a = 4;
+    double b = 6;
+    System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " = " + area(a, b));
+
+  }
+
+  public static void hello(String somebody) {
+    System.out.println("Hello, " + somebody + "!");
+  }
+
+  public static double area(double l) {
+    return l * l;
+  }
+
+  public static double area(double a, double b) {
+    return a * b;
   }
 
 }
