69e875f15700dd72f67922b95a05cb1500c970b8
vitvitvit <vitvitvit82@mail.ru>
2016-03-01 23:34:08 +0300

    Полностью переработано и добавлены классы

---
+++ sandbox/src/main/java/ru/stqa/ptf/sandbox/Rectangle.java
@@ -0,0 +1,18 @@
+package ru.stqa.ptf.sandbox;
+
+/**
+ * Created by inwi on 01.03.2016.
+ */
+public class Rectangle {
+  public double a;
+  public double b;
+
+  public Rectangle(double a, double b) {
+    this.a = a;
+    this.b = b;
+  }
+
+  public double area() {
+    return this.a * this.b;
+  }
+}
---
+++ sandbox/src/main/java/ru/stqa/ptf/sandbox/Square.java
@@ -0,0 +1,18 @@
+package ru.stqa.ptf.sandbox;
+
+/**
+ * Created by inwi on 01.03.2016.
+ */
+public class Square {
+
+  public double l;
+
+  public Square(double l) {
+    this.l = l;
+  }
+
+  public double area() {
+    return this.l * this.l;
+  }
+
+}
