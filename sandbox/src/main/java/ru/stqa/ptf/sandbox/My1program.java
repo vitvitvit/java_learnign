6ea6888d04bbcb12c955f4e8df5b0a801b6a5265
vitvitvit <vitvitvit82@mail.ru>
2016-03-01 22:05:29 +0300

    misspeling

--- sandbox/src/main/java/ru/stqa/ptf/sandbox/My1program.java
+++ sandbox/src/main/java/ru/stqa/ptf/sandbox/My1program.java
@@ -3,7 +3,7 @@ package ru.stqa.ptf.sandbox;
 public class My1program {
 
   public static void main(String[] args) {
-    System.out.println("Hellow, world!");
+    System.out.println("Hello, world!");
   }
 
 }
