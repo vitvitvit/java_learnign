8b4bd83f8a8f655665048175560a2828241a0ed9
vitvitvit <vitvitvit82@mail.ru>
2016-03-03 22:51:20 +0300

    задание №2. 4-ый пункт
    переделан 1-3 пункты, частично изменено

--- sandbox/src/main/java/homework2/DistanceBetweenPoints2D.java
+++ sandbox/src/main/java/homework2/DistanceBetweenPoints2D.java
@@ -7,22 +7,11 @@ public class DistanceBetweenPoints2D {
 
   public static void main(String[] args) {
 
-    Point p1 = new Point();
-    p1.a = 1;
-    p1.b = 2;
+    Point p = new Point(1, 2, 4, 8);
 
-    Point p2 = new Point();
-    p2.a = 4;
-    p2.b = 8;
-
-    System.out.println("Координаты точки A = (" + p1.a + ";" + p1.b + ")");
-    System.out.println("Координаты точки B = (" + p2.a + ";" + p2.b + ")");
-    System.out.println("Расстояние между точками = " + distance(p1, p2));
-  }
-
-  public static double distance(Point p1, Point p2) {
-    double x = Math.pow(p2.a - p1.a, 2) + Math.pow(p2.b - p1.b, 2);
-    return Math.sqrt(x);
+    System.out.println("Координаты точки A = (" + p.a1 + ";" + p.b1 + ")");
+    System.out.println("Координаты точки B = (" + p.a2 + ";" + p.b2 + ")");
+    System.out.println("Расстояние между точками = " + p.distance());
   }
 
 }
--- sandbox/src/main/java/homework2/Point.java
+++ sandbox/src/main/java/homework2/Point.java
@@ -4,6 +4,21 @@ package homework2;
  * Created by inwi on 01.03.2016.
  */
 public class Point {
-  public int a;
-  public int b;
+  public int a1;
+  public int b1;
+  public int a2;
+  public int b2;
+
+  public Point(int a1, int b1, int a2, int b2) {
+    this.a1 = a1;
+    this.b1 = b1;
+    this.a2 = a2;
+    this.b2 = b2;
+  }
+
+  public double distance() {
+    double x = Math.pow(this.a2 - this.a1, 2) + Math.pow(this.b2 - this.b1, 2);
+    return Math.sqrt(x);
+  }
+
 }
