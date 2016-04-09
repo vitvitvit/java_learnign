e9bb738ccebc4eb2ab9ef82ab79f40879db68141
vitvitvit <vitvitvit82@mail.ru>
2016-04-09 19:46:22 +0300

    Вынесен метод
    Вынесена проверка что создана хоть одна группа, контакт в метод 5.2

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactDeletionTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactDeletionTest.java
@@ -1,6 +1,7 @@
 package ru.stqa.pft.addressbook.tests;
 
 import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 
@@ -8,12 +9,16 @@ import java.util.List;
 
 public class ContactDeletionTest extends TestBase {
 
-  @Test
-  public void testContactDeletion() {
+  @BeforeMethod
+  public void ensurePreconditions() {
     app.getNavigationHelper().openHomePage();
     if (! app.getContactHelper().isThereAContact()) {
       app.getContactHelper().createContact(new ContactData("firstname6", "middlename", "lastname", "nickname", "company", "address", "homephone", "mobilephone", null));
     }
+  }
+
+  @Test
+  public void testContactDeletion() {
     List<ContactData> before = app.getContactHelper().getContactList();
     app.getContactHelper().selectContact(before.size() - 1);
     app.getContactHelper().clickDeleteContact();
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTest.java
@@ -1,6 +1,7 @@
 package ru.stqa.pft.addressbook.tests;
 
 import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 import java.util.Comparator;
@@ -8,12 +9,16 @@ import java.util.List;
 
 public class ContactModificationTest extends TestBase{
 
-  @Test
-  public void testContactModification(){
+  @BeforeMethod
+  public void ensurePreconditions() {
     app.getNavigationHelper().openHomePage();
     if (! app.getContactHelper().isThereAContact()) {
       app.getContactHelper().createContact(new ContactData("firstname6", "middlename", "lastname", "nickname", "company", "address", "homephone", "mobilephone", null));
     }
+  }
+
+  @Test
+  public void testContactModification(){
     List<ContactData> before = app.getContactHelper().getContactList();
     app.getContactHelper().clickEditContact(before.size() - 1);
     ContactData contact = new ContactData(before.get(before.size() - 1).getId(),"firstnamemod2", "middlename", "lastname", "nickname", "company", "address", "homephone", "mobilephone", "workphone");
